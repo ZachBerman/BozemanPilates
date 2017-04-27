a4357294906dff0218347f021f35355b7cd01a21
Clark Corey <ccrift@gmail.com>
2017-04-26 21:57:17 -0600

    Adding contact info to footer

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
         <div class="row">
           <div class="col-md-4">
           <br>
-            <span>Studio X</span><br>
-            <span>123 Fake Street</span><br>
-            <span>Bozeman, Mt 59718</span>
+            <span>Livingston Pilates</span><br>
+            <span>110 West Callender St</span><br>
+            <span>Livingston, MT 59047</span>
           </div>
           <div class="col-md-4">
             <ul class="list-inline social-buttons">
@@ -17,15 +17,14 @@
               </li>
               <li><a href="#"><i class="fa fa-facebook"></i></a>
               </li>
-              <li><a href="#"><i class="fa fa-twitter"></i></a>
-              </li><br><br>
+              <br>
               <span class="copyright">Copyright &copy; Studio X</span>
             </ul>
           </div>
           <div class="col-md-4">
             <br>
-            <span><a href="mailto:StudioX@whoknows.com">StudioX@whoknows.com</a></span><br>
-            <span><a href="tel:1-222-222-2222">(222) 222-2222</a></span>
+            <span><a href="mailto:info@livingstonpilates.com">info@livingstonpilates.com</a></span><br>
+            <span><a href="tel:1-406-579-9993">(406)-579-9993</a></span>
           </div>
         </div>
       </div>
--- header.php
+++ header.php
@@ -6,8 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+     <!-- <meta name="description" content="">
+    <meta name="author" content=""> -->
 
     <title><?= get_the_title(); ?> | <?= bloginfo('description'); ?></title>
     <?php wp_head(); ?>
